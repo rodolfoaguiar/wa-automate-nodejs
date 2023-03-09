f85c4a365afdaa4085856c1c9485eadfa4b3b894
Rodolfo Aguiar <rodolfo.z.a@gmail.com>
2023-03-09 17:16:10 -0300

    1.0.96-hub

--- dist/controllers/launch_checks.js
+++ dist/controllers/launch_checks.js
@@ -1,18 +1,18 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function (o, m, k, k2) {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-        desc = { enumerable: true, get: function () { return m[k]; } };
+      desc = { enumerable: true, get: function() { return m[k]; } };
     }
     Object.defineProperty(o, k2, desc);
-}) : (function (o, m, k, k2) {
+}) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function (o, v) {
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
     Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function (o, v) {
+}) : function(o, v) {
     o["default"] = v;
 });
 var __importStar = (this && this.__importStar) || function (mod) {
@@ -42,12 +42,11 @@ const lodash_uniq_1 = __importDefault(require("lodash.uniq"));
 const fs_extra_1 = require("fs-extra");
 const fs = __importStar(require("fs"));
 const pkg = (0, fs_extra_1.readJsonSync)(path.join(__dirname, '../../package.json'));
-// const currentHash = '8d3a09fe3156605ac2cf55ce920bbbab';
+const currentHash = '8d3a09fe3156605ac2cf55ce920bbbab';
 function checkWAPIHash() {
     return __awaiter(this, void 0, void 0, function* () {
         const h = yield hasha_1.default.fromFile(path.join(__dirname, '../lib', 'wapi.js'), { algorithm: 'md5' });
-        // return h == currentHash;
-        return true;
+        return h == currentHash;
     });
 }
 exports.checkWAPIHash = checkWAPIHash;
